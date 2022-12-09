7bcddc11f8a55fb686053aa5f52cae534984eb36
caitie farrell <caitiemfarrell@gmail.com>
2022-12-09 17:17:57 -0500

    added some commented instructions
    transport, intake,

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import org.team555.frc.command.commandrobot.ManagerSubsystemBase;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends ManagerSubsystemBase
+{
+    /*
+     *Requires:
+     *    - One CanSparkMax
+     *          - port: Constants.INTAKE_MOTOR_PORT
+     *          - use MotorType.kBrushless 
+     *    - static final double Constants.INTAKE_SPEED
+     * 
+     * The intake is essentially a 'Ball Sucker'.
+     * When you run the intake, a motor should spin around
+     * which should allow the robot to suck in a ball located
+     * in front of it.
+     * 
+     * An intake should do
+     */
+
+     //void startIntake(): make motor spin with speed Constants.INTAKE_SPEED
+     //void startReverseIntake(): make motor spin backwards with speed Constants.INTAKE_SPEED
+     //void stop(): stop the motor
+
+     //@Override
+     //void whenInactive(): make sure the motor is NOT running
+    
+}
+
+//INSIDE OF Robot.java
+// make it so that 
+//         - when you hit X_Square, it starts sucking
+//         - when you hit B_CIRCLE, it sucks in reverse 
+//
--- src/main/java/frc/robot/subsystems/Transport.java
+++ src/main/java/frc/robot/subsystems/Transport.java
@@ -1,8 +1,35 @@
 package frc.robot.subsystems;
 
+import org.team555.frc.command.commandrobot.ManagerSubsystemBase;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Transport extends SubsystemBase
+public class Transport extends ManagerSubsystemBase
 {
+    /*
+     *Requires:
+     *    - One TalonFX
+     *          - port: Constants.TRANSPORT_MOTOR_PORT 
+     *    - static final double Constants.INTAKE_SPEED
+     * 
+     * The transport is essentially a 'Ball Mover'.
+     * When you run the transport, a motor should spin around
+     * which should move a belt which moves balls from the bottom,
+     * next to the intake, to the top, next to shooter
+     * 
+     * An intake should do
+     */
 
+     //void startMoving(): make motor spin with speed Constants.BALL_TRANSPORT_SPEED
+     //void startMovingBackwards(): make motor spin backwards with speed Constants.BALL_TRANSPORT_SPEED
+     //void stop(): stop the motor
+     // use ControlMode.PercentOutput
+
+     //@Override
+     //void whenInactive(): make sure the motor is NOT running
 }
+
+//In Robot.java
+//make it so that
+//      - When the DPad up, start moving
+//      - When the DPad down, move it down
