0911473d0b3afa67b7e14efae846cc4ea942ca71
team5555 <engineering@montclairrobotics.org>
2023-01-11 16:02:58 -0500

    Commented Intake.java
    Weeeeee

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,41 +10,27 @@ import frc.robot.Constants;
 
 public class Intake extends ManagerSubsystemBase
 {
-    /*
-     *Requires:
-     *    - One CANSparkMax
-     *          - port: Constants.INTAKE_MOTOR_PORT
-     *          - use MotorType.kBrushless 
-     *    - static final double Constants.INTAKE_SPEED
-     * 
-     * The intake is essentially a 'Ball Sucker'.
-     * When you run the intake, a motor should spin around
-     * which should allow the robot to suck in a ball located
-     * in front of it.
-     */
-    private final CANSparkMax motor = new CANSparkMax(Constants.INTAKE_MOTOR_PORT, MotorType.kBrushless);
-     
-     //void startIntake(): make motor spin with speed Constants.INTAKE_SPEED
-     //void startReverseIntake(): make motor spin backwards with speed Constants.INTAKE_SPEED
-     //void stop(): stop the motor
+   // Creates the intake motor
+   private final CANSparkMax motor = new CANSparkMax(Constants.INTAKE_MOTOR_PORT, MotorType.kBrushless);
 
-     public void startIntake()
-     {
-        motor.set(Constants.INTAKE_SPEED);
-     }
-     public void startReverseIntake()
-     {
-        motor.set(-Constants.INTAKE_SPEED);
-     }
-     public void stop()
-     {
-        motor.set(0);
-     }
+   /**
+    * Starts the intake with the speed {@index Constants.INTAKE_SPEED}
+    */
+   public void startIntake()
+   {
+      motor.set(Constants.INTAKE_SPEED);
+   }
+   
+   public void startReverseIntake()
+   {
+      motor.set(-Constants.INTAKE_SPEED);
+   }
+   
+   public void stop()
+   {
+      motor.set(0);
+   }
     
 }
 
-//INSIDE OF Robot.java
-// make it so that 
-//         - when you hit X_Square, it starts sucking
-//         - when you hit B_CIRCLE, it sucks in reverse 
-//
+
